bbe715df378f863111b8b9d06f18548fd8591972
Ann Ndiga <annndiga@gmail.com>
2023-10-03 18:56:12 +0300

    createdd the backend

---
+++ server/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, current_app, make_response, request, jsonify, g
+from dbconfig import db 
+from flask_cors import CORS
+
+from flask_migrate import Migrate
+import os 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///restaurant.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+CORS(app)
